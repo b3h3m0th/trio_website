c787eadff5fd4911a8e9551d6a74f3e53d9f82c3
Behemoth <simonostini@gmail.com>
2022-02-02 17:24:33 +0100

    added shit

--- apps/trio/src/app/app.tsx
+++ apps/trio/src/app/app.tsx
@@ -1,27 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@trio-website/api-interfaces';
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './app.module.scss';
+
+import pipe from '../assets/img/pipe.png';
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
   return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to trio!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
-      </div>
-      <div>{m.message}</div>
-    </>
+    <div>
+      <Tilt>
+        <img src={pipe} alt="neon pipe" />
+      </Tilt>
+    </div>
   );
 };
 
