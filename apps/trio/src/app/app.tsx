dd7e757317936cd88dc956db5fa36e8a016031b2
Behemoth <simonostini@gmail.com>
2022-02-02 17:28:32 +0100

    first tries with 3d tilt

--- apps/trio/src/app/app.tsx
+++ apps/trio/src/app/app.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
-import './app.module.scss';
+import './app.scss';
 
 import pipe from '../assets/img/pipe.png';
 
 export const App = () => {
   return (
-    <div>
+    <div className="app">
       <Tilt>
         <img src={pipe} alt="neon pipe" />
       </Tilt>
