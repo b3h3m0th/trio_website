2fd421041fdad3302ee65bad683aa36e5e57eb88
Behemoth <simonostini@gmail.com>
2022-02-03 14:59:13 +0100

    update

--- libs/flyer/src/lib/flyer.tsx
+++ libs/flyer/src/lib/flyer.tsx
@@ -38,6 +38,29 @@ export const Flyer = (props: FlyerProps) => {
           <h2 className="flyer__content__info__subtitle">
             Alle Guten Dinge <br /> sind drei
           </h2>
+          <div className="flyer__content__info__date">
+            <span className="flyer__content__info__date__day">23</span>
+            <div className="flyer__content__info__date__data">
+              <div className="flyer__content__info__date__data__month">
+                April
+              </div>
+              <div className="flyer__content__info__date__data__location">
+                Altes Hallenbad
+              </div>
+            </div>
+          </div>
+          <div className="flyer__content__info__data">
+            Einlass 20:00 | 16+
+            <br />
+            VVK 12€ | Abendkasse 15€
+            <br />
+            spark7 Ermäßigung -2€
+          </div>
+          <div className="flyer__content__info__dj">
+            <img src="assets/img/avo.png" alt="AVO" />
+            <img src="assets/img/datjay.png" alt="Datjay" />
+            <img src="assets/img/dj_spicy.png" alt="DJ Spicy" />
+          </div>
         </div>
       </div>
     </div>
